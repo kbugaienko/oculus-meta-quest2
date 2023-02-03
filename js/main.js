3aef1f570e52356f4bb1a1d74f80416dc5c2bb29
Khrystyna <kr.bugaienko@gmail.com>
2023-02-03 15:59:44 +0100

    Modify section features
    * Add animation effect when switching partitions
    * Through JS, we created a function to switch sections and correctly display the content for each section from the list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-$(function(){
+'use strict'
 
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element){
+  element.addEventListener('click', opencontent);
 });
+
+function opencontent(evnt) {
+  // found clicked element
+  const tabTarget = evnt.currentTarget;
+
+  // remember what is written in his attribute 'data-button'
+  const button = tabTarget.dataset.button;
+
+  // when clicking, we removed the corresponding classes from everyone with the designation "active"
+  tabItem.forEach(function(item) {
+    item.classList.remove('tabs__btn-item--active');
+  });
+
+  tabContent.forEach(function(item) {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  // added the class "active" to the clicked element
+  tabTarget.classList.add('tabs__btn-item--active');
+
+  // find section by id and add a class with "active"
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
